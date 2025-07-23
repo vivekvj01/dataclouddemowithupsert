819e2edff7ecdfa16b6ce503b7a795c1b76aae01
Vivek Viswanathan <vivek.viswanathan@salesforce.com>
2025-07-23 10:35:10 -0700

    fix: use console.log for logging

--- index.js
+++ index.js
@@ -16,21 +16,20 @@ app.get('/', (req, res) => {
 
 app.post('/api/bookings', async (req, res) => {
     const { guestName } = req.body;
-    const { logger, sdk } = req;
-    logger.info('getting guest name' + guestName);
+    const { sdk } = req;
+    console.log('getting guest name: ' + guestName);
 
     if (!guestName) {
         return res.status(400).json({ error: 'Guest name is required' });
     }
-logger.info('Getting before trying to query');
+    console.log('Getting before trying to query');
 
     try {
-    logger.info('getting developer name ');
+        console.log('getting developer name');
         const orgName = process.env.DATA_CLOUD_ORG_DEVELOPER_NAME;
-        logger.info('gettomg SDK');
+        console.log('getting SDK');
         const appLinkAddon = sdk.addons.applink;
-        logger.info('gettomg SDK');
-        logger.info(`Getting '${orgName}' org connection from Heroku AppLink add-on...`);
+        console.log(`Getting '${orgName}' org connection from Heroku AppLink add-on...`);
         const org = await appLinkAddon.getAuthorization(orgName);
 
         const query = `
@@ -39,13 +38,12 @@ logger.info('Getting before trying to query');
             JOIN ssot__Individual__dlm ON Reservation__dlm.Contact_ID__c = ssot__Individual__dlm.ssot__Id__c
             WHERE ssot__Individual__dlm.ssot__FirstName__c || ' ' || ssot__Individual__dlm.ssot__LastName__c = '${guestName}'
         `;
-        
-        logger.info(`Querying org '${orgName}' (${org.id}): ${query}`);
-        const response = await org.dataApi.query(query);
-        logger.info(`Query response: ${JSON.stringify(response.data || {})}`);
-        res.json(response.data);
+
+        const response = await org.dataCloudApi.query(query);
+        console.log(`Query response: ${JSON.stringify(response)}`);
+        res.json(response);
     } catch (error) {
-        logger.error('Error querying Data Cloud:', error);
+        console.error('Error querying Data Cloud:', error);
         res.status(500).json({ error: 'Failed to fetch bookings from Data Cloud' });
     }
 });
